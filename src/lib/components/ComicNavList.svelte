a4123037b62b4e6cbd3170d73351ba25d7b01f5f
ranaldsgift <kevinjohncampbell@gmail.com>
2023-01-24 02:16:14 -0500

    Change comic nav list to not use perspective/3d

--- src/lib/components/ComicNavList.svelte
+++ src/lib/components/ComicNavList.svelte
@@ -3,45 +3,10 @@
 </script>
 
 <style>
-    a.btn::before {
-        background-image: url('/images/overlays/monochrome.jpg');
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-size: 121%;
-        background-repeat: no-repeat;
-        box-shadow: rgb(129 140 248 / 86%) 1px 1px 1px 38px inset;
-        background-position: -13px -123px;
-    }
-    a.btn::after {
-        position: absolute;
-        content: attr(data-title);
-        text-shadow: 2px 2px black;
-        display: grid;
-        align-content: center;
-        left: 2rem;
-        color: #ffffff;
-    }
-    a.btn:nth-child(2n+2)::after {
-        justify-content: end;
-        right: 2rem;
-    }
-    a.btn:nth-child(2n+2) {
-        transform: perspective(100px) rotatey(-1deg);
-    }
-    a.btn {
-        position: relative;
-        transform: perspective(100px) rotatey(1deg);
-        min-height: 48px;
-    }
     .comic-list-nav {
         color: #ffffff;
     }
     .comic-list-nav a {
-        width: 80%;
         border: 3px solid black;
         border-radius: 0;
         background-size: 121%;
@@ -50,7 +15,6 @@
         background-position: -13px -123px;
         text-align: left;
         justify-content: start;
-        color: #00000000;
         text-transform: uppercase;
         font-size: 2rem;
         font-family: 'Badaboom';
@@ -59,13 +23,57 @@
     .comic-list-nav a:nth-child(2n+2) {
         justify-self: end;
     }
+    
+    .halftone-container {
+	--size: 0.75rem;
+        --mask: linear-gradient(to left, rgb(0 0 0), rgb(0 0 0 / 0.5));
+	--dotsColor: 255 255 255;
+	--bg: radial-gradient(circle at center, rgb(var(--dotsColor)) 3%, rgb(var(--dotsColor) / 0.4) var(--dotRadius, 150%));
+	--bgSize: var(--size);
+	--cardBg: linear-gradient(135deg, hotpink, blue);
+        background: var(--cardBg);
+        overflow: hidden;
+        position: absolute;
+        width: 200%;
+        height: 100%;
+        opacity: 0.1;
+
+    }
+    .halftone {
+        position: relative;
+        aspect-ratio: 1;
+        background: rgb(0 0 0);
+        filter: contrast(var(--contrast, 20)) invert(var(--invert, 0));
+        mix-blend-mode: var(--blendMode, multiply);
+        width: 100%;
+        height: 100%;
+    }
+
+    .halftone::before {
+        content: '';
+        position: absolute;
+        inset: 0;
+        background-image: var(--bg);
+        background-size: var(--bgSize, 0.75rem) var(--bgSize, 0.75rem);
+        background-repeat: round;
+        background-position: 0 0, var(--bgPosition) var(--bgPosition);
+        -webkit-mask-image: var(--mask);
+        mask-image: var(--mask);
+        mask-size: cover;
+        mask-position: center;
+        transition: background-size 100ms;
+    }
 </style>
 
 <div class="m-auto">
     <nav class="grid m-auto gap-4 px-8 max-w-7xl comic-list-nav">
         {#each listItems as listItem}
-        <a href="{listItem.url}/" class="grid btn" data-title={listItem.name}>
-        </a>
+        <div class="">
+            <a href="{listItem.url}" class="grid btn relative overflow-hidden text-stroke" data-title={listItem.name}>
+                {listItem.name}
+                <div class="halftone-container"><div class="halftone"></div></div>
+            </a>
+        </div>
         {/each}
     </nav>
 </div>
